0ccc7b884bc87ff4cf830766949cd4884cacbe6b
mohamedRebai <mohamedrebai748@gmail.com>
2024-05-01 17:23:55 +0100

    merged

--- Interfaces/window2.py
+++ Interfaces/window2.py
@@ -5,7 +5,7 @@ from PyQt5.QtWidgets import QMessageBox
 
 parent_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(parent_dir)
-# from Scheduling.model import schedule
+from Scheduling.model import schedule
 
 tasks = []
 resources = []
