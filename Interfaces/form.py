53b4b0fe7b8f95033ac2c8205781a12df5c6768d
AminaGhannem <amina.ghannem@insat.ucar.tn>
2024-05-06 21:38:43 +0200

    fixed the interfaces

--- Interfaces/form.py
+++ Interfaces/form.py
@@ -2,12 +2,11 @@ import sys
 from PyQt5 import QtWidgets, uic
 from window1 import Window1
 from window2 import Window2
-import os
-dir_path = os.path.dirname(os.path.realpath(__file__)) 
+
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MyWindow, self).__init__()
-        uic.loadUi(os.path.join(dir_path, 'form.ui'), self)
+        uic.loadUi('./form.ui', self)
         self.button1.clicked.connect(self.on_button1_click)
         self.button2.clicked.connect(self.on_button2_click)
 
@@ -19,8 +18,7 @@ class MyWindow(QtWidgets.QMainWindow):
         self.window2 = Window2()
         self.window2.show()
 
-def startApplication():
-    app = QtWidgets.QApplication(sys.argv)
-    window = MyWindow()
-    window.show()
-    sys.exit(app.exec_())
+app = QtWidgets.QApplication(sys.argv)
+window = MyWindow()
+window.show()
+sys.exit(app.exec_())
